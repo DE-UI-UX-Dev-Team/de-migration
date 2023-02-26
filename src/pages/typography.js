cab29ae73e067d856cb45ab498cced592294e809
nrg-juzzell <joshua.uzzell@nrg.com>
2023-02-26 12:44:39 -0500

    DM-47 DM-5 Adds a button to toggle between white and dark backgrounds to test contrast of text

--- src/pages/typography.js
+++ src/pages/typography.js
@@ -1,1 +1,14 @@
-import '../styles/main.scss';
+import '../styles/main.scss'; 
+
+(function() {
+    const contrastButton = document.getElementById( 'contrast' ); 
+    const sectionElements = document.querySelectorAll( 'section' );
+
+    contrastButton.addEventListener( 'click', ( e ) => {
+        console.log( e );
+        for( let i = 0; i < sectionElements.length; i++ ) { 
+            sectionElements[ i ].classList.toggle( 'bg--primary-color' );
+        }
+
+    });
+})();
