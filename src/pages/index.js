63f5d9114d2dae46d0f58854976cddd9de7d067a
Anusha Saripella <anusha.saripella@directenergy.com>
2023-03-01 16:12:46 -0600

    [DM-61] Adding the linear gradients and animated curves

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 import '../styles/main.scss';
-import '../assets/img/de_main_curve.svg'
-import '../assets/img/de_main_curve_blue.svg'
+import '../assets/img/de_main_curve.svg';
+import '../assets/img/de_main_curve_blue.svg';
+import '../assets/img/de_main_curve_orange.svg';
+import '../assets/img/de_animated_curve_orange.svg';
+import '../assets/img/de_animated_curve_blue.svg';
