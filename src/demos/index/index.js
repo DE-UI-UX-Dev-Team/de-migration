f333ced748f670124851824eee795262d595d755
nrg-juzzell <joshua.uzzell@nrg.com>
2023-03-08 21:11:47 -0500

    Add Roboto font assets to the project

--- src/demos/index/index.js
+++ src/demos/index/index.js
@@ -1,5 +1,25 @@
 import '../../styles/main.scss'; 
 
+/* Fonts */
+import '../../assets/fonts/roboto/roboto-bold-webfont.woff';
+import '../../assets/fonts/roboto/roboto-bolditalic-webfont.woff';
+import '../../assets/fonts/roboto/roboto-italic-webfont.woff';
+import '../../assets/fonts/roboto/roboto-light-webfont.woff';
+import '../../assets/fonts/roboto/roboto-lightitalic-webfont.woff';
+import '../../assets/fonts/roboto/roboto-medium-webfont.woff';
+import '../../assets/fonts/roboto/roboto-mediumitalic-webfont.woff';
+import '../../assets/fonts/roboto/roboto-regular-webfont.woff';
+
+import '../../assets/fonts/roboto/roboto-bold-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-bolditalic-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-italic-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-light-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-lightitalic-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-medium-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-mediumitalic-webfont.woff2';
+import '../../assets/fonts/roboto/roboto-regular-webfont.woff2';
+
+
 /* Images */
 import '../../assets/images/de_main_curve.svg';
 import '../../assets/images/de_main_curve_blue.svg';
--- webpack.config.js
+++ webpack.config.js
@@ -79,6 +79,13 @@ module.exports = ( env ) => {
           generator: {
             filename: 'assets/images/[name][ext]'
           }
+        },
+        {
+          test: /\.(woff|woff2)$/,  
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/fonts/[name][ext]'
+          }
         }
       ],
     },
