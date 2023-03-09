9dcfe7dfa629c464289640db9625ae22add94752
nrg-juzzell <joshua.uzzell@nrg.com>
2023-03-08 20:57:25 -0500

    Move asset and main scss includes to main index file

---
+++ src/demos/index/index.js
@@ -0,0 +1,8 @@
+import '../../styles/main.scss'; 
+
+/* Images */
+import '../../assets/images/de_main_curve.svg';
+import '../../assets/images/de_main_curve_blue.svg';
+import '../../assets/images/de_main_curve_orange.svg';
+import '../../assets/images/de_animated_curve_orange.svg';
+import '../../assets/images/de_animated_curve_blue.svg';
