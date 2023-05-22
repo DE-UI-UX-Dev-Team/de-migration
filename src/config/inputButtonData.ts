9cf0b979f69aa271d53ac43002f5be9420600cd3
Anusha Saripella <anusha.saripella@directenergy.com>
2023-05-22 11:18:33 -0500

    feature/storybook - Buttons typescript component and story

---
+++ src/config/inputButtonData.ts
@@ -0,0 +1,24 @@
+import { InputButtonProps } from '../interfaces/Props';
+
+const buttonData: InputButtonProps[] = [
+    {
+        inputButtonType: 'submit',
+        inputButtonValue: 'Submit Input',
+        inputButtonIconLeft: '',
+        inputButtonIconRight: '',
+    },
+    {
+        inputButtonType: 'reset',
+        inputButtonValue: 'Reset Input',
+        inputButtonIconLeft: '',
+        inputButtonIconRight: '',
+    },
+    {
+        inputButtonType: 'button',
+        inputButtonValue: 'Button Input',
+        inputButtonIconLeft: '',
+        inputButtonIconRight: '',
+    },
+];
+
+export default buttonData;
