e334d27b1b61e3b77370a61b407e755e9d666549
Anusha Saripella <anusha.saripella@directenergy.com>
2023-07-12 13:02:03 -0500

    [DM-51] Removed the jquery and refactored the code in the TabComponent. created functions to handle the scroll events.

--- src/components/Tabs/TabsComponent.tsx
+++ src/components/Tabs/TabsComponent.tsx
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import $ from 'jquery';
-
+import React, { useState, useEffect, useRef } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import { TabProps } from '../../interfaces/Props';
 
@@ -11,87 +9,106 @@ interface TabsProps {
 const TabsComponent: React.FC<TabsProps> = ({ tabs }) => {
     const [tabIndex, setTabIndex] = useState(0);
     const [tabState, setTabState] = useState({
-        showleft: true,
-        showright: true,
+        showLeft: true,
+        showRight: true,
     });
+
     const scrollDuration: number = 300;
+
+    let tabListRef = useRef<HTMLDivElement | null>(null);
+
+    const handleTabScroll = () => {
+        if (tabListRef.current) {
+            const tabListElement = tabListRef.current.querySelector('.react-tabs__tab-list') as HTMLElement;
+            if (tabListElement) {
+                const menuWrapperSize = tabListElement.offsetWidth;
+                let menuPosition = tabListElement.scrollLeft;
+                menuPosition = menuPosition ? menuPosition : 0;
+                const itemElement = tabListElement.querySelector('li.react-tabs__tab') as HTMLLIElement;
+                const itemSize = itemElement?.offsetWidth;
+                const menuSize: number = tabs.length * itemSize;
+                let menuInvisibleSize: number = menuSize - (menuWrapperSize ? menuWrapperSize : 0);
+                const menuEndOffset = menuInvisibleSize - 20;
+                let showLeft = false;
+                let showRight = false;
+                if (menuPosition <= 20) {
+                    showLeft = false;
+                    showRight = true;
+                } else if (menuPosition < menuEndOffset) {
+                    showLeft = true;
+                    showRight = true;
+                } else if (menuPosition >= menuEndOffset) {
+                    showLeft = true;
+                    showRight = false;
+                }
+                setTabState({
+                    showLeft,
+                    showRight,
+                });
+            }
+        }
+    };
+
+    const scrollTabs = (left: boolean) => {
+        console.log('test', tabListRef);
+        if (tabListRef.current) {
+            const tabListElement = tabListRef.current.querySelector(
+                '.tab-list-wrapper .react-tabs__tab-list'
+            ) as HTMLElement;
+            if (tabListElement && tabListElement.offsetWidth) {
+                const menuPosition = tabListElement.scrollLeft;
+                const menuWrapperSize = tabListElement.offsetWidth || 0;
+                const targetScrollLeft = menuPosition + (left ? -menuWrapperSize : +menuWrapperSize);
+                if (menuWrapperSize) {
+                    tabListElement.scrollTo({
+                        left: targetScrollLeft,
+                        behavior: 'smooth',
+                    });
+                }
+            }
+        }
+    };
 
     return (
         <Tabs
             selectedIndex={tabIndex}
-            onSelect={(index) => setTabIndex(index)}
+            onSelect={setTabIndex}
         >
-            <TabList
-                onScroll={(e) => {
-                    const menuWrapperSize = $(e.currentTarget).outerWidth();
-                    let menuPosition = $(e.currentTarget).scrollLeft();
-                    console.log('test ' + menuPosition);
-                    console.log();
-                    menuPosition = menuPosition ? menuPosition : 0;
-                    const itemSize: any = $('li.react-tabs__tab').outerWidth(true);
-                    const menuSize: number = tabs.length * itemSize;
-                    let menuInvisibleSize: number = menuSize - (menuWrapperSize ? menuWrapperSize : 0);
-                    const menuEndOffset = menuInvisibleSize - 20;
-                    let showLeft = false;
-                    let showright = false;
-                    if (menuPosition <= 20) {
-                        showLeft = false;
-                        showright = true;
-                    } else if (menuPosition < menuEndOffset) {
-                        showLeft = true;
-                        showright = true;
-                    } else if (menuPosition >= menuEndOffset) {
-                        showLeft = true;
-                        showright = false;
-                    }
-                    setTabState({
-                        showleft: showLeft,
-                        showright: showright,
-                    });
-                }}
+            <div
+                className="tab-list-wrapper"
+                ref={tabListRef}
             >
-                {tabs.map((tabItem) => (
-                    <Tab>
-                        <i className={tabItem.tabIcon}></i>
-                        <span>{tabItem.tabTitle}</span>
-                    </Tab>
-                ))}
-                <div className="indicators">
-                    <a
-                        className={tabState.showleft ? 'indicator indicator--left' : 'indicator indicator--left hidden'}
-                        onClick={(e) => {
-                            let parent = e.currentTarget?.parentElement?.parentElement as HTMLElement;
-                            let menuPosition = $(parent).scrollLeft();
-                            menuPosition = menuPosition ? menuPosition : 0;
-                            let menuWrapperSize = $(parent).outerWidth();
-                            menuWrapperSize = menuWrapperSize ? menuWrapperSize : 0;
-                            $(parent).animate({ scrollLeft: menuPosition - menuWrapperSize }, scrollDuration);
-                        }}
-                    >
-                        <i className="fa-solid fa-chevron-left "></i>
-                    </a>
+                <TabList onScrollCapture={() => handleTabScroll()}>
+                    {tabs.map((tabItem, index) => (
+                        <Tab key={index}>
+                            <i className={tabItem.tabIcon}></i>
+                            <span>{tabItem.tabTitle}</span>
+                        </Tab>
+                    ))}
+                    <div className="indicators">
+                        <a
+                            className={
+                                tabState.showLeft ? 'indicator indicator--left' : 'indicator indicator--left hidden'
+                            }
+                            onClick={() => scrollTabs(true)}
+                        >
+                            <i className="icon-md fa-solid fa-chevron-left"></i>
+                        </a>
 
-                    <a
-                        className={
-                            tabState.showright ? 'indicator indicator--right' : 'indicator indicator--right hidden'
-                        }
-                        onClick={(e) => {
-                            let parent = e.currentTarget?.parentElement?.parentElement as HTMLElement;
-                            let menuPosition = $(parent).scrollLeft();
-                            console.log('test ' + menuPosition);
-                            menuPosition = menuPosition ? menuPosition : 0;
-                            let menuWrapperSize = $(parent).outerWidth();
-                            menuWrapperSize = menuWrapperSize ? menuWrapperSize : 0;
-                            $(parent).animate({ scrollLeft: menuPosition + menuWrapperSize }, scrollDuration);
-                        }}
-                    >
-                        <i className="fa-solid fa-chevron-right"></i>
-                    </a>
-                </div>
-            </TabList>
+                        <a
+                            className={
+                                tabState.showRight ? 'indicator indicator--right' : 'indicator indicator--right hidden'
+                            }
+                            onClick={() => scrollTabs(false)}
+                        >
+                            <i className="icon-md fa-solid fa-chevron-right"></i>
+                        </a>
+                    </div>
+                </TabList>
+            </div>
 
-            {tabs.map((tabPanelItem) => (
-                <TabPanel>{tabPanelItem.tabContent}</TabPanel>
+            {tabs.map((tabPanelItem, index) => (
+                <TabPanel key={index}>{tabPanelItem.tabContent}</TabPanel>
             ))}
         </Tabs>
     );
