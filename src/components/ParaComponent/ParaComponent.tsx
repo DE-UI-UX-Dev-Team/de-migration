84d018e27f20c4b5693bad28339423cb71b55c6a
Anusha Saripella <anusha.saripella@directenergy.com>
2023-07-28 13:55:57 -0500

    [DM-51] removed the para component which effected the accordion component. so used sample component in accordion instead of para as its been removed

--- src/components/ParaComponent/ParaComponent.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ParaProps } from '../../interfaces/Props';
-
-export const ParaComponent: React.FC<ParaProps> = ({ text }) => {
-    return <p>{text}</p>;
-};
-
-export default ParaComponent;
