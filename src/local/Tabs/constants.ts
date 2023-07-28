96900207c5fdbf960003fbc3773ebdd0a8bf3817
Anusha Saripella <anusha.saripella@directenergy.com>
2023-07-28 13:59:50 -0500

    [DM-51] Created a new file under locals for storing Tab constants

---
+++ src/local/Tabs/constants.ts
@@ -0,0 +1,17 @@
+export const tabTitles = {
+    TabHorizontal1: 'HorizontalTabs With Icon',
+    TabHorizontal2: 'HorizontalTabs Without Icon',
+    TabVertical1: 'Vertical Tabs Without Icon',
+    TabVertical2: 'Vertical Tabs With Icon',
+    TabColumns: 'Tab with Column Content and TextBlock',
+    TabBilling: 'Tab with Billing Content and TextBlock',
+};
+
+export const tabBlockContent = {
+    billingBlockHead: 'How Do We Calculate Your Average Bill: An Example',
+    billingBlockPara:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem. Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed. Donec placerat nisl magna, et faucibus arcu condimentum sed.Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+    utilityBlockHead: 'Utility Companies in Texas',
+    ultilityBlockPara:
+        'Copy about why you need to select a utility provider (if you need to turn power on at a new address) or know your current utility provider (if you are transferring service to a new address). The utility charges are the same for all residents in that utilitys service area.',
+};
