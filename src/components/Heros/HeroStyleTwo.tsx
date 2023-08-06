e125c1502474642189203805ac31787a348d36d8
SusanRuanNRG <Susan.Ruan@nrg.com>
2023-08-06 16:34:57 -0400

    Use single SVG to implement responsive design

--- src/components/Heros/HeroStyleTwo.tsx
+++ src/components/Heros/HeroStyleTwo.tsx
@@ -1,24 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import TextInput from '../Forms/TextInput';
-import { DesktopSvg, MobileSvg, TabletSvg } from './HeroSvg';
+import React from 'react';
+import { OptionTwoSvg } from './HeroSvg';
 
 const HeroStyleTwo: React.FC = () => {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const isMobileScreen = windowWidth < 768;
-    const isTabletScreen = windowWidth < 992;
-
     return (
         <div className="hero-style-two">
             <div className="hero-content-style-two row">
@@ -54,7 +37,7 @@ const HeroStyleTwo: React.FC = () => {
                         src="/assets/images/hero-img.jpg"
                         alt="Hero"
                     />
-                    {isMobileScreen ? <MobileSvg /> : isTabletScreen ? <TabletSvg /> : <DesktopSvg />}
+                    <OptionTwoSvg />
                 </div>
             </div>
         </div>
