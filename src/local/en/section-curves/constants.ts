5075a6213e1a23f4aa2d716968538b0272dee32c
SusanRuanNRG <Susan.Ruan@nrg.com>
2023-08-11 12:05:16 -0400

    Create section curve component

---
+++ src/local/en/section-curves/constants.ts
@@ -0,0 +1,27 @@
+export const clickBtn = 'click me';
+
+export const content = {
+    descOne:
+        'We empower our customers with tools and resources, like our usage insights tool and energy saving tips, to stay savvy about their energy use. Our partnership with Hive takes this even further with smart home devices that make it easy to save. Daily habits are an important part of energy-efficiency in your home, and our energy + Hive bundles help you incorporate these electricity-saving tasks to reduce your bills from wherever you are.',
+    descTwo:
+        'This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of <strong>user style sheets</strong>. You can use it for various other purposes as well,like just checking how your browser displays various HTML elements by default. It can also be useful when testing conversions from HTML format to other formats, since some elements can go wrong then.',
+    descThree:
+        'This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.',
+    descFour:
+        ' This is a normal paragraph (<code>p</code> element). To add some length to it, let us mention that this page was primarily written for testing the effect of <strong>user style sheets</strong>.',
+};
+
+export const imageInfo = {
+    srcOne: 'https://awue1-s3-de-oam-cms-assets-prod-prod-001.s3.amazonaws.com/s3fs-public/2022-05/amazon_lady2_1.png',
+    altOne: 'image of woman enjoying amazon prime',
+    srcTwo: 'https://d38suhqqynrvtl.cloudfront.net/images/photos/ls_computer_O_02.png',
+    altTwo: 'some description',
+    srcThree: 'https://d38suhqqynrvtl.cloudfront.net/images/photos/ls_livingroom_05.jpg',
+    altThree: 'home size',
+    srcFour: 'https://awue1-s3-de-oam-cms-assets-prod-prod-001.s3.amazonaws.com/s3fs-public/2023-02/amazon-hero.png',
+    altFour: 'amzon prime benefits image',
+    srcFive: 'https://s3.amazonaws.com/upload.uxpin/files/912828/903124/family-v2-9aeeda.png',
+    altFive: 'some description',
+    srcSix: 'https://d38suhqqynrvtl.cloudfront.net/images/photos/pr_echo_home_white.png',
+    altSix: 'some description',
+};
